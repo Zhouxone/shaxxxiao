1a15ba8b684c3ef024fd9ce96deb21c63baef38b
rin <rinaldychou.k@gmail.com>
2025-06-22 21:07:22 +0800

    mengubah file shaxxxiao

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odit nemo incidunt, sed itaque molestiae dignissimos facere illo doloremque harum excepturi est voluptate at sit rem ducimus ipsam commodi, ratione minima.</p>
+    <h1>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quam nihil, asperiores eligendi repellat vel exercitationem laborum quos nulla, voluptatibus sed minus quisquam. Iusto ipsam maiores dignissimos facere aliquam deserunt. Omnis.</h1>
 </head>
 <body>
     
