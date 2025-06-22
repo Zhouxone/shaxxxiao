1e9e29d69ba9cea0e4700038f43e1fbe35c724b9
rin <rinaldychou.k@gmail.com>
2025-06-22 21:06:04 +0800

    menambahkan file index

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odit nemo incidunt, sed itaque molestiae dignissimos facere illo doloremque harum excepturi est voluptate at sit rem ducimus ipsam commodi, ratione minima.</p>
+</head>
+<body>
+    
+</body>
+</html>
