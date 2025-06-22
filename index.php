fa682b426f57677b431951496647e98f7ab92bc1
rin <rinaldychou.k@gmail.com>
2025-06-22 21:18:17 +0800

    mengubah apapapun

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odit nemo incidunt, sed itaque molestiae dignissimos facere illo doloremque harum excepturi est voluptate at sit rem ducimus ipsam commodi, ratione minima.</p>
-    <h1>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quam nihil, asperiores eligendi repellat vel exercitationem laborum quos nulla, voluptatibus sed minus quisquam. Iusto ipsam maiores dignissimos facere aliquam deserunt. Omnis.</h1>
+    <h2>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quam nihil, asperiores eligendi repellat vel exercitationem laborum quos nulla, voluptatibus sed minus quisquam. Iusto ipsam maiores dignissimos facere aliquam deserunt. Omnis.</h2>
 </head>
 <body>
     
